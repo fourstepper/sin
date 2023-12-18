8289c8c06e3d3e8862408d39b7b7a590b54ac330
Robin Opletal <me@robinopletal.com>
2023-12-18 15:48:34 +0100

    embed templates

--- main.go
+++ main.go
@@ -1,31 +1,35 @@
 package main
 
 import (
+	"embed"
 	"log"
 	"net/http"
 
 	"html/template"
 )
 
+//go:embed views/*.html
+var tmpl embed.FS
+
 func main() {
 	r := http.NewServeMux()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("view/index.html", "view/base.html")
+		tmpl, err := template.ParseFS(tmpl, "views/index.html", "views/base.html")
 		if err != nil {
 			log.Fatal(err)
 		}
 		tmpl.ExecuteTemplate(w, "base", "data")
 	})
 	r.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("view/about.html", "view/base.html")
+		tmpl, err := template.ParseFiles("views/about.html", "views/base.html")
 		if err != nil {
 			log.Fatal(err)
 		}
 		tmpl.ExecuteTemplate(w, "base", "data")
 	})
 	r.HandleFunc("/second-about", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("view/second-about.html", "view/base.html")
+		tmpl, err := template.ParseFiles("views/second-about.html", "views/base.html")
 		if err != nil {
 			log.Fatal(err)
 		}
