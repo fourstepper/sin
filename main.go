733bdbc3a6375f3d3257cdafda8281e18034b0df
Robin Opletal <me@robinopletal.com>
2023-12-18 15:34:20 +0100

    move from chi to stdlib

--- main.go
+++ main.go
@@ -5,36 +5,32 @@ import (
 	"net/http"
 
 	"html/template"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	r := chi.NewRouter()
+	r := http.NewServeMux()
 
-	log.Println("Started")
-	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.ParseFiles("view/index.html", "view/base.html")
 		if err != nil {
 			log.Fatal(err)
 		}
 		tmpl.ExecuteTemplate(w, "base", "data")
 	})
-	r.Get("/about", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.ParseFiles("view/about.html", "view/base.html")
 		if err != nil {
 			log.Fatal(err)
 		}
 		tmpl.ExecuteTemplate(w, "base", "data")
 	})
-	r.Get("/second-about", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/second-about", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.ParseFiles("view/second-about.html", "view/base.html")
 		if err != nil {
 			log.Fatal(err)
 		}
 		tmpl.ExecuteTemplate(w, "base", "data")
 	})
+	log.Println("Listening...")
 	http.ListenAndServe(":3000", r)
 }
