6da345578721448377a0cca814ae556befbca854
Robin Opletal <me@robinopletal.com>
2023-12-26 15:19:44 +0100

    feat(css): playing with customizing css

--- main.go
+++ main.go
@@ -12,23 +12,31 @@ import (
 var tmpl embed.FS
 
 func main() {
-	r := http.NewServeMux()
+	mux := http.NewServeMux()
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// https://lets-go.alexedwards.net/sample/02.08-serving-static-files.html#the-http-fileserver-handler
+	staticDir := http.FileServer(http.Dir("static"))
+	mux.Handle("/static/", http.StripPrefix("/static", staticDir))
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			http.NotFound(w, r)
+			return
+		}
 		tmpl, err := template.ParseFS(tmpl, "views/index.html", "views/base.html")
 		if err != nil {
 			log.Fatal(err)
 		}
 		tmpl.ExecuteTemplate(w, "base", "data")
 	})
-	r.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.ParseFiles("views/about.html", "views/base.html")
 		if err != nil {
 			log.Fatal(err)
 		}
 		tmpl.ExecuteTemplate(w, "base", "data")
 	})
-	r.HandleFunc("/second-about", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/second-about", func(w http.ResponseWriter, r *http.Request) {
 		tmpl, err := template.ParseFiles("views/second-about.html", "views/base.html")
 		if err != nil {
 			log.Fatal(err)
@@ -36,5 +44,5 @@ func main() {
 		tmpl.ExecuteTemplate(w, "base", "data")
 	})
 	log.Println("Listening...")
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(":3000", mux)
 }
